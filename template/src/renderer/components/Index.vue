fe740caa35dcdae03f3fb03a523770e8d99739eb
梁乐 <lowleung@163.com>
2020-07-31 23:59:05 +0800

    修正模板

---
+++ template/src/renderer/components/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <h1>Index</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data(){
+      return{
+
+      }
+    },
+    methods: {
+
+    },
+    mounted(){
+
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    background:radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
