3ccfec8a191a63e37078fac22f1a4f378525f831
梁乐 <lowleung@163.com>
2020-08-01 00:20:53 +0800

    修正模板

--- template/src/main/index.js
+++ template/src/main/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 {{/if_eq}}
-import { app, BrowserWindow } from 'electron' {{#if_eq eslintConfig 'airbnb'}} // eslint-disable-line{{/if_eq}}
+import { app, BrowserWindow } from 'electron'{{#if_eq eslintConfig 'airbnb'}} // eslint-disable-line{{/if_eq}}
 
 const NodeRSA = require('node-rsa');
 var regedit = require('regedit')
@@ -40,150 +40,149 @@ MFL1DOXGfi+GxShH8RSJghLxwEYx1glTuMxVgQawgJ4=
 -----END RSA PRIVATE KEY-----`;
 function checkLicense(success, fail) {
   try {
-        if (fs.existsSync(root)) {
-          var licenceEncrypted = fs.readFileSync(root, 'utf8').toString();
-          var _privateKey = new NodeRSA(privateKey);
-          var decrypted = _privateKey.decrypt(licenceEncrypted, 'utf8');
-          var jsons = JSON.parse(decrypted)
-          if (jsons['forever']) {
+    if (fs.existsSync(root)) {
+      var licenceEncrypted = fs.readFileSync(root, 'utf8').toString();
+      var _privateKey = new NodeRSA(privateKey);
+      var decrypted = _privateKey.decrypt(licenceEncrypted, 'utf8');
+      var jsons = JSON.parse(decrypted)
+      if (jsons['forever']) {
+        updateLast(jsons)
+        success()
+      } else {
+        if (jsons['last'] > Date.now()) {
+          fail()
+        } else {
+          if (jsons['timeline'] > Date.now()) {
             updateLast(jsons)
             success()
           } else {
-            if (jsons['last'] > Date.now()) {
-              fail()
-            } else {
-              if (jsons['timeline'] > Date.now()) {
-                updateLast(jsons)
-                success()
-              } else {
-                fail()
-              }
-            }
+            fail()
           }
-
-        } else {
-          check_regedit().then((res) => {
-            if (res) {
-              var tmp = JSON.stringify({
-                forever: false,
-                timeline: new Date().setDate(new Date().getDate() + 30),
-                last: Date.now(),
-              });
-              var _publicKey = NodeRSA(publicKey)
-              var decrypted = _publicKey.encrypt(tmp, 'base64');
-              fs.writeFileSync(root, decrypted)
-              success()
-            } else {
-              fail()
-            }
-          })
         }
       }
-      catch (err) {
-        fail()
-      }
+
+    } else {
+      check_regedit().then((res) => {
+        if (res) {
+          var tmp = JSON.stringify({
+            forever: false,
+            timeline: new Date().setDate(new Date().getDate() + 30),
+            last: Date.now(),
+          });
+          var _publicKey = NodeRSA(publicKey)
+          var decrypted = _publicKey.encrypt(tmp, 'base64');
+          fs.writeFileSync(root, decrypted)
+          success()
+        } else {
+          fail()
+        }
+      })
+    }
+  }
+  catch (err) {
+    fail()
+  }
 }
 
 function updateLast(json) {
-      json.last = new Date()
-      var _publicKey = NodeRSA(publicKey)
-      var decrypted = _publicKey.encrypt(json, 'base64');
-      fs.writeFileSync(root, decrypted)
+  json.last = new Date()
+  var _publicKey = NodeRSA(publicKey)
+  var decrypted = _publicKey.encrypt(json, 'base64');
+  fs.writeFileSync(root, decrypted)
 }
 
 function queue_software() {
-      return new Promise((resolve, reject) => {
-        regedit.list(['HKLM\\SOFTWARE'], function (err, SOFTWARE) {
-          if (err !== null) return reject(err)
-          resolve(SOFTWARE)
-        })
-      })
+  return new Promise((resolve, reject) => {
+    regedit.list(['HKLM\\SOFTWARE'], function (err, SOFTWARE) {
+      if (err !== null) return reject(err)
+      resolve(SOFTWARE)
+    })
+  })
 }
 function queue_company() {
-      return new Promise((resolve, reject) => {
-        regedit.list(['HKLM\\SOFTWARE\\ChunKun'], function (err, COMPANY) {
-          if (err !== null) return reject(err)
-          resolve(COMPANY)
-        })
-      })
+  return new Promise((resolve, reject) => {
+    regedit.list(['HKLM\\SOFTWARE\\ChunKun'], function (err, COMPANY) {
+      if (err !== null) return reject(err)
+      resolve(COMPANY)
+    })
+  })
 }
 function create_company() {
-      return new Promise((resolve, reject) => {
-        regedit.createKey(['HKLM\\SOFTWARE\\ChunKun'], function (err) {
-          if (err !== null) resolve(false)
-          resolve(true)
-        })
-      })
+  return new Promise((resolve, reject) => {
+    regedit.createKey(['HKLM\\SOFTWARE\\ChunKun'], function (err) {
+      if (err !== null) resolve(false)
+      resolve(true)
+    })
+  })
 }
 function insert_product() {
-      return new Promise((resolve, reject) => {
-        regedit.putValue({ 'HKLM\\SOFTWARE\\ChunKun': data }, function (err) {
-          if (err !== null) resolve(false)
-          resolve(true)
-        })
-      })
+  return new Promise((resolve, reject) => {
+    regedit.putValue({ 'HKLM\\SOFTWARE\\ChunKun': data }, function (err) {
+      if (err !== null) resolve(false)
+      resolve(true)
+    })
+  })
 }
 async function check_regedit() {
-      var SOFTWARE = await queue_software();
-      if (SOFTWARE['HKLM\\SOFTWARE'].hasOwnProperty('keys') && SOFTWARE['HKLM\\SOFTWARE'].keys.indexOf('ChunKun') > -1) {
-        var COMPANY = await queue_company();
-        if (COMPANY['HKLM\\SOFTWARE\\ChunKun'].hasOwnProperty('values') && COMPANY['HKLM\\SOFTWARE\\ChunKun'].values.hasOwnProperty(name)) {
-          return false
-        } else {
-          await insert_product();
-          return true
-        }
-      } else {
-        await create_company();
-        await insert_product();
-        return true
-      }
+  var SOFTWARE = await queue_software();
+  if (SOFTWARE['HKLM\\SOFTWARE'].hasOwnProperty('keys') && SOFTWARE['HKLM\\SOFTWARE'].keys.indexOf('ChunKun') > -1) {
+    var COMPANY = await queue_company();
+    if (COMPANY['HKLM\\SOFTWARE\\ChunKun'].hasOwnProperty('values') && COMPANY['HKLM\\SOFTWARE\\ChunKun'].values.hasOwnProperty(name)) {
+      return false
+    } else {
+      await insert_product();
+      return true
+    }
+  } else {
+    await create_company();
+    await insert_product();
+    return true
+  }
 }
 
-    /**
-     * Set `__static` path to static files in production
-     * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
-     */
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
 if (process.env.NODE_ENV !== 'development') {
-      global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\'){ {#if_eq eslintConfig 'airbnb' } } // eslint-disable-line{{/if_eq}}
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\'){{#if_eq eslintConfig 'airbnb'}} // eslint-disable-line{{/if_eq}}
 }
-
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
 
 function createWindow() {
-      /**
-       * Initial window options
-       */
-      checkLicense(
-        function () {
-          mainWindow = new BrowserWindow({
-            height: 1080,
-            useContentSize: true,
-            width: 1920,
-            x: 0,
-            y: 0,
-            frame: false,
-            center: true,
-            alwaysOnTop: true,
-            fullscreen: true,
-            kiosk: true,
-            webPreferences: {
-              webSecurity: false
-            }
-          })
-
-          mainWindow.loadURL(winURL)
-
-          mainWindow.on('closed', () => {
-            mainWindow = null
-          })
-        },
-        function () {
-          app.quit()
-          mainWindow = null
+  /**
+   * Initial window options
+   */
+  checkLicense(
+    function () {
+      mainWindow = new BrowserWindow({
+        height: 1080,
+        useContentSize: true,
+        width: 1920,
+        x: 0,
+        y: 0,
+        frame: false,
+        center: true,
+        alwaysOnTop: true,
+        fullscreen: true,
+        kiosk: true,
+        webPreferences: {
+          webSecurity: false
         }
-      )
+      })
+
+      mainWindow.loadURL(winURL)
+
+      mainWindow.on('closed', () => {
+        mainWindow = null
+      })
+    },
+    function () {
+      app.quit()
+      mainWindow = null
+    }
+  )
 }
 
 app.on('ready', createWindow)
@@ -202,22 +201,20 @@ app.on('activate', () => {
 {{#if_eq builder 'builder'}}
 
 /**
-* Auto Updater
-     *
-     * Uncomment the following code below and install `electron-updater` to
-     * support auto updating. Code Signing with a valid certificate is required.
-     * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
-     */
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
 
-    /*
-    import { autoUpdater } from 'electron-updater'
-    
-    autoUpdater.on('update-downloaded', () => {
-      autoUpdater.quitAndInstall()
-    })
-    
-    app.on('ready', () => {
-      if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-    })
-*/
+/*
+import { autoUpdater } from 'electron-updater'
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
 {{/if_eq}}
